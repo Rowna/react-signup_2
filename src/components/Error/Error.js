2f8f4d4e695ca1b013bbe78f3c88c88e5e3f3b56
Rowan kh <rowan.kharnoub@adesso.de>
2022-01-03 09:05:51 +0100

    w: Kommentare

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -4,9 +4,7 @@ import React from 'react';
 function Error(props) {
     return (
         <div>
-            {props.errors[props.name] === false && (
-                <p>Bitte füllen Sie dieses Feld aus.</p>
-            )}
+            {props.errors[props.name] === false && ( <p>Bitte füllen Sie dieses Feld aus.</p> ) }
         </div>
     )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,20 +8,34 @@ import Button from './Button/Button';
 import './SignUp.css';
 
 
-function validateForm(values) {
+// function validateForm(values) {
+const validateForm = (values) => {
        const errors = {};
 
+       // hier wird geprüft: Ist die Länge von values.firstname >= 3?
+       // das Ergebnis ist entweder true oder false.
+       // Dieses Ergebnis wird dann errors.firstname zugewiesen. 
        errors.firstname = values.firstname.length >= 3;
+       // hier wird geprüft: Ist die Laenge von values.lastname >= 3, 
+       // Das Ergebnis ist false oder true,
+       // Dann wird das Ergebnis errors.lastname zugewiesen
        errors.lastname = values.lastname.length >= 3;
+       // Es wird geprüft: Ist die Länge von values.email >=3 ?
+       // Das Ergebnis ist entweder false oder true,
+       // Dann wird das Ergebnis errors.email zugewiesen
        errors.email = values.email.length >= 3;
-       errors.street = values.email.length >= 3;
+       // Es wird geprüft: Ist die Länge von values.email >=3?
+       // Das Ergebnis ist entweder true oder false 'bool',
+       // Dann wird das Ergebnis errors.street zugewiesen
+       errors.street = values.street.length >= 3;
 
-       return errors; // {"firstname":false,"lastname":false,"email":false,"street":false}
+       return errors; // { "firstname": false,   "lastname": false,    "email": false,  "street": false }
 }
+
 
 const SignUp = () => {
 
-       const initialValues = {
+       const eintraege = {
                             firstname: '', 
                             lastname: '', 
                             email: '', 
@@ -30,17 +44,53 @@ const SignUp = () => {
                             postcode: '', 
                             country: ''
                      };
-                     
-       const [formValues, setFormValues] = useState(initialValues);
+       // wir brauchen state-variablen, um React mitzuteilen,
+       // wann es (d.h. React) ein bestimmtes Element/Component
+       // updaten soll -- naemlich dann, wenn sich die state-Variable
+       // aendert!
+       //
+       // Heisst im Umkehrschluss: Wenn wir WOLLEN, dass React ein bestimmtes Update
+       // vornimmt, muessen wir ihm dafuer eine state-Variable mit auf den
+       // Weg geben. Sonst erkennt React seinen Auftrag nicht.
+       const [formValues, setFormValues] = useState(eintraege);
        const [errors, setErrors] = useState({});
        const [buttonDisabled, setButtonDisabled] = useState(true);
      
        const formChangeHandler = (event) => {
+              // wir isolieren event.target.name und event.target value
+              // und speichern sie in den NEUEN variablen 'name' und 'value' ab.
+              // Fachbegriff: Object-Decomposition
               const { name, value } = event.target;
+
+              // {...formValues, [name]: value } bedeutet:
+              // nimm alle bestehenden eintraege von formValues,
+              // speichere sie als neue eintrage fuer ein neues {object},
+              // und ueberschreibe dann den eintrag [name]: eintrag mit [name]: value.
+              // die Schreibweise [name] ist eine Sicherheits-Schreibweise, falls 
+              // 'name' (Z.50) zufaelligerweise mal 'last-name' heissen sollte, also
+              // Zeichen enthaelt, die JavaScript nur dann als key akzeptiert, wenn
+              // sie ausdruecklich als String zu erkennen sind: "last-name"
+              // Bei [name] wird der Wert von 'name' (Z.50) automatisch in einen String
+              // umgewandelt. 
               const nextValues = {...formValues, [name]: value };
+              // console.log(nextValues);
+
+              // hier wird das alte objekt 'formValues', z.B. mit der Speicheradresse 11111
+              // durch das NEUE Objekt nextValues mit der Speicheradresse 22222 ersetzt.
+              // formValues ist nur eine Referenz auf eine Speicheradresse.
+              // setFormValues ersetzt die alte Speicheradresse 11111 durch die NEUE
+              // Speicheradresse 22222.
+              // Das ist der Unterschied, den React "sieht". Neue Speicheradressen-Eintrag
+              // in formValues. Darauf reagiert es und updatet das Component.
+              // Das ist extrem wichtig zu verstehen, damit du die Arbeitsweise von React verstehst!
               setFormValues(nextValues);
-              // => einige errors werden gesetzt, andere bleiben leer
-              // das liegt am rueckgabewert von validateForm()
+              // wichtige Beobachtung: Bei validateForm() "mit Klammern" wird die Funktion ausgefuehrt,
+              // bei validateForm ohne Klammern nicht. validateForm speichert die Speicheradresse
+              // der Funktion validateForm. Wenn validateForm() ausgefuehrt wird, ist der Wert,
+              // der in setErrors() ankommt, nicht die Speicheradresse von validateForm,
+              // sondern die Speicheradresse von ihrem RÜCKGABEWERT!!!
+              // genau diese Speicheradresse ist jetzt der neue Wert der State-Variablen "errors"
+              // weil React diese Werte-Änderung mitbekommt, wird dieses Component upgedatet.
               setErrors(validateForm(nextValues));
        }
       
@@ -54,15 +104,16 @@ const SignUp = () => {
                        type="text"
                        placeholder='Vorname'
                        name='firstname'
+                       // {!errors.firstname === false}: mit '!' vergleichen wir, ob firstname im Objekt false ist, aber errors{} 
+                       // ist ein leeres Objekt, und errors.firstname ist undefined und gibt es noch nicht. Deswegen nachdem es gibt, 
+                       // vergleichen wir es, ob es === false. 
+                     
                        hasError={errors.firstname === false}
                        value={formValues.firstname}
                        onChangeHandler={formChangeHandler}
                 />
                 {/* {errors.firstname === false && <p>Vorname ist erforderlich</p> } */}
-                <Error 
-                       errors={errors} 
-                       name="firstname"        
-                />
+                <Error errors={errors} name="firstname" />
                 </InputWrapper>
 
                 <InputWrapper gridPosition="lastname-input">
